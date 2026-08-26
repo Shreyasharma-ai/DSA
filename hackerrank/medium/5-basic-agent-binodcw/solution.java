740894a549dc6da75962ce137daa574484856506
Shreya sharma <shreyasharam2007@gmail.com>
2026-08-26 20:17:16 +0530

    ✅ 5. Basic - Agent Binod(CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ hackerrank/medium/5-basic-agent-binodcw/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String agentname=sc.nextLine();
+        System.out.println("Hi my name is Agent " + agentname);
+        
+        sc.close();
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
