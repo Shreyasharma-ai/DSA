e112c71ce7c9b1d0af7ca35026108c8192b0d204
Shreya sharma <shreyasharam2007@gmail.com>
2026-08-26 20:29:39 +0530

    ✅ 7. Basic - Area & Perimeter of Circle(CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ hackerrank/medium/7-basic-area-perimeter-of-circlecw/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+
+        long r = sc.nextLong();
+        long pi = 3;
+
+        long area = pi * r * r;
+        long perimeter = 2 * pi * r;
+
+        System.out.println(area);
+        System.out.println(perimeter);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
