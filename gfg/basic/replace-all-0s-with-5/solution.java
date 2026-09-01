5b1db25b3c781790e6c4f0a6c14fb5e4d45f1189
Shreya Sharma <shreyasharam2007@gmail.com>
2026-09-01 09:45:24 +0530

    ✅ Replace all 0's with 5 | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ gfg/basic/replace-all-0s-with-5/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    int reverse(int n) {
+        int rev = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+
+            if (digit == 0)
+                digit = 5;
+
+            rev = rev * 10 + digit;
+            n /= 10;
+        }
+
+        return rev;
+    }
+
+    public int convertFive(int n) {
+        if (n == 0)
+            return 5;
+
+        return reverse(reverse(n));
+    }
+}
