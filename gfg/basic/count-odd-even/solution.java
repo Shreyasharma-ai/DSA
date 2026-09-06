c7fb7eaa2608e687d1bf8a5fa0457481942864e1
Shreya Sharma <shreyasharam2007@gmail.com>
2026-09-06 10:18:56 +0530

    ✅ Count Odd and Even | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ gfg/basic/count-odd-even/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countOddEven(int[] arr) {
+        // Code here
+       int odd=0;
+       int even=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==1)
+            odd++;
+            else
+            even++;
+        }
+        int[]result={odd,even};
+        return result;
+    }
+}
