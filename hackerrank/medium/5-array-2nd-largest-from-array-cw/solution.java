5a4ea80d02574fd9b6ce769464fb2d2ce284694e
Shreya Sharma <shreyasharam2007@gmail.com>
2026-09-23 09:54:49 +0530

    ✅ 5. Array - 2nd Largest from array (CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ hackerrank/medium/5-array-2nd-largest-from-array-cw/solution.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void SecondLargest(int[] arr, int n) {
+        // Write code here
+        int largest=arr[0];
+        int SecondLargest=1;
+         
+         for(int i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                SecondLargest = largest;
+                largest=arr[i];
+            }
+            else if(arr[i]>SecondLargest && arr[i]<largest){
+                SecondLargest=arr[i];
+                
+            }
+         
+         }
+        System.out.println(SecondLargest);
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int[] arr= new int[n];
+
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+            
+        SecondLargest(arr,n);
+        sc.close();
+        
+    }
+}
